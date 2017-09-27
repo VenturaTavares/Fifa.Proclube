0c87ae4678fe319fc987e5084ab473e7e63de131
Bruno Ventura Tavares <bruno_ventura1@hotmail.com>
2017-09-27 11:16:05 -0300

    Inclusão de clases de contexto e repositório

---
+++ Fifa.Proclube.Domains/Repositorio/CampeonatoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿
+using Fifa.Proclube.Domains.Infraestrutura;
+using Fifa.Proclube.Domains.Models;
+
+namespace Fifa.Proclube.Domains.Repositorio
+{
+    public class CampeonatoRepositorio : BaseRepository<Campeonato>
+    {
+        public CampeonatoRepositorio(ProclubeContext context) : base(context)
+        {
+
+        }
+    }
+
+
+}
